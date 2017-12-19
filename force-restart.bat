3c4c4eef1951754302691ab68b77de69ffca3d4b
cpoc-dev <34671316+cpoc-dev@users.noreply.github.com>
2017-12-19 02:27:33 -0500

    changed time to 2am

--- force-restart.bat
+++ force-restart.bat
@@ -1,2 +1,2 @@
-schtasks /create /ru SYSTEM /sc weekly /d sat /st 23:00 /tn force-restart /tr "c:\windows\system32\shutdown.exe /r /f /t 00"
+schtasks /create /ru SYSTEM /sc weekly /d sun /st 2:00 /tn force-restart /tr "c:\windows\system32\shutdown.exe /r /f /t 00"
 schtasks /run /tn "force-restart"
